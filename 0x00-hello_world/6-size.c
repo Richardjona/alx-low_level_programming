4dd0da695f7cc9b7b0b015cb24d4716c80a5405d
Richardjona <richjo4526@gmail.com>
2022-09-09 05:16:23 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - start point
+ *main - start point
+ *Description: using the main function
+ *Return: set to 0 success
  *
- * Return: set to 0 success
- *
+ * this program prints "programming is like a multilingual puzzle
  */
 int main(void)
 
@@ -12,13 +13,13 @@ int main(void)
 
 	char c;
 	int i;
-	long l;
-	long long ll;
+	long int l;
+	long long int ll;
 	float f;
 
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld byte(s)\n", sizeof(i);
-	printf("size of a long int: %ld byte(s)\n", sizeof(i));
+	printf("size of a long int: %ld byte(s)\n", sizeof(l));
 	printf("size of a long long int: %ld byte(s)\n", sizeof(ll));
 	printf("size of a float: %ld byte(s)\n", sizeof(f));
 
