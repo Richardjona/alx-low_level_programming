1d0bf73de7051b83f2007ff720b6ae3a90d57738
Richardjona <richjo4526@gmail.com>
2022-09-14 12:39:44 +0000

    even

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - print sum of even fibonacci
- * Return: 0
- */
+*main - print sum of even fibonacci
+*Return: 0
+*/
 
 int main(void)
 {
-	int sum_of_evens = 0
+	int sum_of_evens = 0;
 	int a;
 	int b;
 	int sum = 1;
+
 	a = 1;
 	b = 1;
+
 
 	while (b < 4000000)
 	{
 		sum = a + b;
-		a = b
+		a = b;
+		b = sum;
 		if ((sum <= 4000000) && (sum % 2 == 0))
-		sum_of_events += sum;
+		sum_of_evens += sum;
 	}
-	printf("%d\n", sum_of_events);
+	printf("%d\n", sum_of_evens);
 	return (0);
 }
