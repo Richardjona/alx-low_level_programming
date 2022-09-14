c980f37a6c7e2d92ca73bf6d84b304fd21226332
Richardjona <richjo4526@gmail.com>
2022-09-14 06:14:45 +0000

    fibonac

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,18 +8,18 @@
 int main(void)
 
 {
-	int count;
-	int fifty = 50;
+	int counter;
+	int countto = 50;
 	long a = 1;
 	long b = 2;
 
-	for (count = 1; count <= (fifty / 2); count++)
+	for (counter = 1; counter <= (countto / 2); counter++)
 	{
 		printf("%li %li ", a, b);
 		a += b;
 		b += a;
 	}
-	if (fifty % 2 == 1)
+	if (countto % 2 == 1)
 		printf("%li", a);
 	printf("\n");
 
