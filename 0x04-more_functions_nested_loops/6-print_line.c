38206606076834ff6352f603a5f5a435d121f8f5
Richardjona <richjo4526@gmail.com>
2022-09-16 16:19:40 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - this function multiplies values
+ *
+ * @n: value
+ */
+
+void print_line(int n)
+{
+	int m;
+
+	if (n < 0)
+	{
+		for (m = 0; m < n; m++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
