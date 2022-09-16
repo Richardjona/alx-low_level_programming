496ce3426ace4a2938d5fcc22d77afb195570be0
Richardjona <richjo4526@gmail.com>
2022-09-16 16:10:18 +0000

    print

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints values excluding
+ * 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if ((n != 2) && (n != 4))
+		{
+			_putchar('0' + n);
+		}
+	}
+	_putchar('\n');
+}
