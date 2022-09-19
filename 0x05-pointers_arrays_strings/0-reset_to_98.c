af6f3e61f64f79a80e19e2d3064d594512cc0577
Richardjona <richjo4526@gmail.com>
2022-09-19 18:38:31 +0000

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/*
+ * reset_to_98 - function updates values to 98
+ * @n: the value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/* this is a putchar function*/
+int _putchar(char c);
+/*function that updates the values it ponts*/
+void reset_to_98(int *n);
+/*function that swaps the value of two pointers*/
+void swap_int(int *a, int *b);
+/*function that returns the length of strings*/
+int _strlen(char *s);
+/*function tha prints a string*/
+void _puts(char *str);
+
+
+#endif
