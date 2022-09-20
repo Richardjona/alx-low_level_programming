822d732e90529d8ed5ecbe26805ee62d4e897c9b
Richardjona <richjo4526@gmail.com>
2022-09-20 04:23:09 +0000

    reverse

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,6 +14,19 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 /*function tha prints a string*/
 void _puts(char *str);
-
+/* function reverses a string */
+void rev_string(char *s);
+/*function that prints every other character of a string */
+void puts2(char *str);
+/* function that prints half of a string followed
+ * by a new line
+ */
+void puts_half(char *str);
+/* function that prints n elements of an array of int */
+void print_array(int *a, int n);
+/* function that copies the string by src */
+char *_strcpy(char *dest, char *src);
+/* functin that converts string to integer */
+int _atoi(char *s);
 
 #endif
