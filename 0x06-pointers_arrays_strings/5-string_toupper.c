b33ae5f0024a49a89c5abfb941e5d771bb7c5d02
Richardjona <richjo4526@gmail.com>
2022-09-22 15:05:41 +0000

    upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * string_toupper - function pass to upper
+ *
+ * @string: dest string
+ * Return: string
+ */
+
+char *string_toupper(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		if (string[i] >= 'a' && string[i] <= 'z')
+		{
+			string[i] -= 32;
+		}
+	}
+	return (string);
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes
+ * @string: the string
+ * Return: string
+ */
+
+char
