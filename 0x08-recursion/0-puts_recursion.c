0c852570183c03925987c3bbc511073dc2bd791e
Richardjona <richjo4526@gmail.com>
2022-09-30 11:59:15 +0000

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#ncllude "main.h"
+
+/**
+ *_puts_recursion - this is a function to print string
+ *recursively
+ *@s: string parameter
+ */
+
+void _puts_recursion(char *s)
+{
+	s = "jon\0"
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++
+	_puts_recursion(s);
+}
+
